b9eef3e3ea00e86de76ea12078649abd73fe74e1
ChevaunBent <chevaunbent@gmail.com>
2020-11-19 22:10:30 -0500

    Exercise 1, 2 & 3 completed: correction to previous commit

--- world.php
+++ world.php
@@ -6,7 +6,6 @@ $dbname = 'world';
 $country = $_GET['country'];
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
-
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
