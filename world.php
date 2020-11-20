de735ab634078b24a164569ca467599a4e733c89
ChevaunBent <chevaunbent@gmail.com>
2020-11-19 23:26:46 -0500

    Exercise 4 completed

--- world.php
+++ world.php
@@ -5,12 +5,24 @@ $password = 'password123';
 $dbname = 'world';
 $country = $_GET['country'];
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+
 $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+echo "<table border ='1'>";
+echo "<tr><th>Name". "</th>". "<th>Continent". "</th>". "<th>Independence". "</th>". "<th>Head of State". "</th></tr>";
+foreach ($results as $row){   //Creates a loop to loop through results
+  echo "<tr><td>" . $row['name']. "</td>". "<td>". $row['continent']. "</td>". "<td>". $row['independence_year']. "</td>". "<td>". $row['head_of_state']. "</td></tr>";
+  }
+echo "</table>"; //Close the table in HTML
+
+
+
+/*
 ?>
-<ul>
+<table>
 <?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
+  <li><?= $row['name']. $row['continent']. $row['independence_year']. $row['head_of_state']; ?></li>
 <?php endforeach; ?>
-</ul>
+</table>
+*/
