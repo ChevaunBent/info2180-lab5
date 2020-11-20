85d35391efefa3ec09d6f11f830b707bfff0553b
ChevaunBent <chevaunbent@gmail.com>
2020-11-19 23:46:05 -0500

    Exercise 5 attempted

--- world.php
+++ world.php
@@ -9,7 +9,7 @@ $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $p
 $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-echo "<table border ='1'>";
+echo "<table border ='1' style='background-color:#FFFFFF;border-collapse:collapse;border:2px solid #6699FF;color:#000000;width:400'>";
 echo "<tr><th>Name". "</th>". "<th>Continent". "</th>". "<th>Independence". "</th>". "<th>Head of State". "</th></tr>";
 foreach ($results as $row){   //Creates a loop to loop through results
   echo "<tr><td>" . $row['name']. "</td>". "<td>". $row['continent']. "</td>". "<td>". $row['independence_year']. "</td>". "<td>". $row['head_of_state']. "</td></tr>";
